0c2ddaefa08ac5d92d435d846c920f47910f61a4
denishadaslani <denishadashlani@gmail.com>
2024-12-03 10:53:46 +0530

    PR-2

---
+++ PR-2/PR2_1_2_.C
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int marks;
+	char grade;
+
+	clrscr();
+	printf("Enter your marks:");
+	scanf("%d",&marks);
+
+
+
+  grade = (marks>=90 && marks<=100)
+				?'A'
+				:(marks>=80 && marks<90)
+							?'B'
+							:(marks>70 && marks<=80)
+										? 'C'
+										:(marks>60 && marks<=70)
+													? 'D'
+													:(marks>50 && marks<=60)
+																?'E'
+																:'F';
+
+printf("your grade is  %c.",grade);
+switch(grade)
+{
+
+		case 'A':
+		case 'a':
+			printf("Excellent work!");
+			break;
+
+		case 'B':
+		case 'b':
+			printf("Well done.");
+			break;
+
+
+		case 'C':
+		case 'c':
+			printf("Good job.");
+			break;
+
+		case 'D':
+		case 'd':
+			printf("you passed,but you could do better.");
+			break;
+
+		case 'E':
+		case 'e':
+			printf("you could do better.");
+			break;
+
+		case 'F':
+		case 'f':
+			printf("soory,you failled.");
+			break;
+
+
+
+     }
+
+   if(grade>='A' && grade<='E')
+    {
+		  printf("you are eligible for the next level..");
+    }
+    else
+    {
+		 printf("please try again next time.. ");
+    }
+
+getch();
+
+
+}
